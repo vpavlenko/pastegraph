2f08082c6653342eabf1fcd80e26d1c2b0a5cdd2
vpavlenko <cxielamiko@gmail.com>
2013-07-07 21:49:38 +0400

    png fixed to svg, hash_ renamed to hash

--- file_keeper/models.py
+++ file_keeper/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class File(models.Model):
-    hash_ = models.CharField(max_length=100, db_index=True, unique=True)
+    hash = models.CharField(max_length=100, db_index=True, unique=True)
     # although sha224 uses only 56 characters
 
     mime = models.CharField(max_length=100)
--- file_keeper/views.py
+++ file_keeper/views.py
@@ -4,5 +4,5 @@ from file_keeper.models import File
 
 
 def get_file(request, hash_):
-    file_ = File.objects.get(hash_=hash_)
+    file_ = File.objects.get(hash=hash_)
     return HttpResponse(file_.base64.decode('base64'), mimetype=file_.mime)
