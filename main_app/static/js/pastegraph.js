8386c04518b4b4ef31cd6596fc565a9783825682
vpavlenko <cxielamiko@gmail.com>
2013-07-09 01:54:17 +0400

    #5, #6: basic support of springify; #10: example of edges list syntax

--- main_app/static/js/pastegraph.js
+++ main_app/static/js/pastegraph.js
@@ -37,25 +37,111 @@ $(document).ready(function() {
         $('#error').html('An error occured during ' + action + ': <br>' + jqXHR.responseText);
     }
 
+    function js_error(report) {
+        $('#error').text(report);
+    }
+
     function clean_errors() {
         $('#error').html('');
     }
 
-    $('#plot-button').click(function() {
+    function switch_to_method(name) {
+        $('#plot-results').show();
+        $('#plot-results > *').hide();
+        $('#' + name + '-plot-result').show();
         $('#error').html('');
-        $('#plot-img').hide();
+    }
+
+    function to_2d_array(lines) {
+        return jQuery.map(lines.split("\n"), function(line) {
+            return [line.split(' ')];
+        });
+    }
+
+    function build_graph() {
+        // Build graph from #source using #graph-options.
+        // Return instance of Springy.Graph class.
+        var graph = new Springy.Graph();
+        var lines = to_2d_array($('#source-textarea').val());
+        var directed = $('input:radio[name=directed]:checked').val() === "true";
+
+        if ($('input:radio[name=source_type]:checked').val() == 'adjacency_matrix') {
+            var min_number = 1;
+            var num_vertices = lines.length;
+
+            for (var i = min_number; i < min_number + num_vertices; ++i) {
+                graph.addNodes(i);
+            }
+
+            for (var i = 0; i < num_vertices; ++i) {
+                if (lines[i].length != num_vertices) {
+                    js_error('Invalid amount of number at line ' + (i + 1) + 
+                            ': expected ' + num_vertices + ', got ' + 
+                            lines[i].length);
+                    return undefined;
+                }
+                for (var j = 0; j < num_vertices; ++j) {
+                    if (lines[i][j] != 0) {
+                        graph.addEdges([min_number + i, min_number + j, {directed: directed}]);
+                    }
+                }
+            }
+        } else {
+            for (var i in lines) {
+                if (lines[i].length != 2) {
+                    js_error('Invalid amount of number at line ' + (parseInt(i, 10) + 1) + 
+                            ': expected 2, got ' + 
+                            lines[i].length);
+                    return undefined;
+                }
+                if (!(lines[i][0] in graph.nodeSet)) {
+                    graph.addNodes(lines[i][0]);
+                }
+                if (!(lines[i][1] in graph.nodeSet)) {
+                    graph.addNodes(lines[i][1]);
+                }
+                graph.addEdges([lines[i][0], lines[i][1], {directed: directed}]);
+            }
+        }
+
+        return graph;
+    }
+
+    $('#graphviz-plot-button').click(function() {
+        switch_to_method('graphviz');
+        
+        $('#graphviz-plot-result').removeClass('ajax-loader-rendering');
+        $('#graphviz-plot-result').removeClass('ajax-loader-loading');
+
+        $('#graphviz-plot-img').hide();
         $('#ajax-loader').show();
+        $('#graphviz-plot-result').addClass('ajax-loader-rendering');
         // $('#plot').hide();
         $.post('plot/', $('#graph-form').serialize())
         .done(function(data, textStatus, jqXHR) {
-            $('#ajax-loader').hide();
             // $('#plot').html($('<img>').attr('src', 'file/get/' + data['image_hash']));
-            $('#plot-img').attr('src', 'file/get/' + data['image_hash']);
-            $('#plot-img').show();
+            $('#graphviz-plot-result').removeClass('ajax-loader-rendering');
+            $('#graphviz-plot-result').addClass('ajax-loader-loading');
+            $('#graphviz-plot-img').attr('src', 'file/get/' + data['image_hash']).load(function() {
+                $('#graphviz-plot-result').removeClass('ajax-loader-loading');
+                $('#ajax-loader').hide();
+                $('#graphviz-plot-img').show();
+            });
         })
         .fail(function(jqXHR, textStatus, errorThrown) {
             $('#ajax-loader').hide();            
             handle_error('sending grah data to plot', jqXHR, textStatus, errorThrown);
         })
+    })
+
+    $('#springy-simple-plot-button').click(function() {
+        switch_to_method('springy-simple');
+        
+        var graph = build_graph();
+        if (graph !== undefined) {
+            $('#springy-simple-canvas').springy({graph: graph});
+        } else {
+            $('#springy-simple-plot-result').hide();
+        }
     })
 })
